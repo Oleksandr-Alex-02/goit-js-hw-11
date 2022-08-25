8b9dc3b74e69e13fe8e5737eb7306b30662e15a5
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-08-25 17:03:24 +0300

    scrol

--- src/index.js
+++ src/index.js
@@ -1,64 +1,66 @@
 // import URL from './url.js';
-import axios from 'axios';
-const axios = require('axios');
+import cards from './templates/cards.hbs';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
+const clickBtn = document.querySelector('.button');
+// const jsGuard = document.querySelector('.js-guard');
 
-searchForm.addEventListener('submit', URL);
-let searchImages = '';
+let page = 0;
+let value = '';
 
-function URL(e) {
-  const URL = 'https://pixabay.com/api/';
-  const KEY = 'key=29453019-5a69b6c7b2f01a070c80deb0c';
-  const OPHIN = `&image_type=photo&orientation=horizontal&safesearch=true`;
+// const options = {
+//   root: null,
+//   rootMargin: '10px',
+//   threshold: 1,
+// };
 
-  searchImages = e.currentTarget.elements.searchQuery.value;
+searchForm.addEventListener('submit', input);
+clickBtn.addEventListener('click', scrol);
+
+function input(e) {
   e.preventDefault();
-  return axios
-    .get(`${URL}?${KEY}${OPHIN}&q=${searchImages}`)
-    .then(res => {
-      markup(res);
-    })
-    .catch(error => {
-      // handle error
-      console.log(error);
-    });
+  value = e.currentTarget.elements.searchQuery.value;
+  htmlMarkup();
+  e.target.reset();
 }
 
-function markup(res) {
-  const aaa = res.data.hits;
-  const markup = aaa
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-        <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item">
-                <b>Likes: ${likes}</b>
-              </p>
-              <p class="info-item">
-                <b>Views: ${views}</b>
-              </p>
-              <p class="info-item">
-                <b>Comments: ${comments}</b>
-              </p>
-              <p class="info-item">
-                <b>Downloads: ${downloads}</b>
-              </p>
-            </div>
-        </div>`;
-      }
-    )
-    .join('');
-  gallery.insertAdjacentHTML('afterbegin', markup);
+function htmlMarkup() {
+  gallery.innerHTML = '';
+  // page = 0;
+  URL((page = 1)).then(data => {
+    const markup = cards(data.hits);
+    gallery.insertAdjacentHTML('beforeend', markup);
+    // observer.observe(jsGuard);
+  });
+}
+
+// const observer = new IntersectionObserver(updateList, options);
+
+// function updateList(entries) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       URL((page += 1)).then(data => {
+//         const markup = cards(data.hits);
+//         gallery.insertAdjacentHTML('beforeend', markup);
+//       });
+//     }
+//   });
+// }
+
+function scrol(e) {
+  URL((page += 1)).then(data => {
+    const markup = cards(data.hits);
+    gallery.insertAdjacentHTML('beforeend', markup);
+  });
+}
+
+function URL(page) {
+  const URL = 'https://pixabay.com/api/';
+  const KEY = 'key=29453019-5a69b6c7b2f01a070c80deb0c';
+  const params = `&image_type=photo&orientation=horizontal&safesearch=true&per_page=9`;
+
+  return fetch(`${URL}?${KEY}&q=${value}${params}&page=${page}`).then(resp =>
+    resp.json()
+  );
 }
--- src/url.js
+++ src/url.js
@@ -1,23 +1,13 @@
-axios.get('/user', {
-  params: {
-    ID: 12345,
-  },
-});
+// import axios from 'axios';
+// const axios = require('axios');
+// import value from './index.js';
 
-function URL(e) {
-  const URL = 'https://pixabay.com/api/';
-  const KEY = 'key=29453019-5a69b6c7b2f01a070c80deb0c';
-  const OPHIN = `&image_type=photo&orientation=horizontal&safesearch=true`;
+// export default function URL(page) {
+//   const URL = 'https://pixabay.com/api/';
+//   const KEY = 'key=29453019-5a69b6c7b2f01a070c80deb0c';
+//   const params = `&image_type=photo&orientation=horizontal&safesearch=true&per_page=9`;
 
-  searchImages = e.currentTarget.elements.searchQuery.value;
-  e.preventDefault();
-  return axios
-    .get(`${URL}?${KEY}${OPHIN}&q=${searchImages}`)
-    .then(res => {
-      markup(res);
-    })
-    .catch(error => {
-      // handle error
-      console.log(error);
-    });
-}
+//   return fetch(`${URL}?${KEY}${params}&q=${value}&page=${page}`).then(resp =>
+//     resp.json()
+//   );
+// }
