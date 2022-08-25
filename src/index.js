afcac07de8b1b894a63987c362b7250e5a901c8c
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-08-25 21:34:32 +0300

    смс

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cards from './templates/cards.hbs';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const clickBtn = document.querySelector('.button');
+const clickBtn = document.querySelector('.load-more');
 // const jsGuard = document.querySelector('.js-guard');
 
 let page = 0;
@@ -28,10 +28,15 @@ function input(e) {
 function htmlMarkup() {
   gallery.innerHTML = '';
   // page = 0;
+
   URL((page = 1)).then(data => {
     const markup = cards(data.hits);
     gallery.insertAdjacentHTML('beforeend', markup);
     // observer.observe(jsGuard);
+    const totalHits = data.totalHits;
+    sms(totalHits);
+    console.log(totalHits);
+    console.log(data);
   });
 }
 
@@ -63,4 +68,16 @@ function URL(page) {
   return fetch(`${URL}?${KEY}&q=${value}${params}&page=${page}`).then(resp =>
     resp.json()
   );
+}
+
+function sms(totalHits) {
+  if (totalHits >= 1) {
+    console.log('Ура нашли котіков');
+  }
+  if (totalHits === 0) {
+    console.log('печалька');
+  }
+  if (page === totalHits) {
+    console.log("We're sorry, but you've reached the end of search results.");
+  }
 }
