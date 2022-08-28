b306407e7fd223efc3fca08abe3656a7a254fc19
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-08-28 13:18:17 +0300

    +++

--- src/index.js
+++ src/index.js
@@ -1,56 +1,73 @@
 import FethApiServes from './url';
 import cards from './templates/cards.hbs';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 Notiflix.Notify.init({
   width: '400px',
   position: 'right-top',
   distance: '10px',
   borderRadius: '50px',
-  // timeout: 1000,
+  timeout: 3000,
 });
 
+const options = {
+  root: null,
+  rootMargin: '200px',
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver(updateList, options);
 const newApiServer = new FethApiServes();
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const clickBtn = document.querySelector('.load-more');
+// const clickBtn = document.querySelector('.load-more'); //btn load-more
 const btnSubmit = document.querySelector('button[type="submit"]');
+const jsGuard = document.querySelector('.js-guard');
 disabledBtnSearch();
 
 searchForm.addEventListener('submit', startScript);
-clickBtn.addEventListener('click', httpsRequest);
+// clickBtn.addEventListener('click', httpsRequest);  //btn load-more
 
 function startScript(e) {
   disabledBtnSearch();
   e.preventDefault();
 
-  clickBtn.classList.remove('is-hiden');
+  // clickBtn.classList.remove('is-hiden');  //btn load-more
   removeGallery();
   newApiServer.query = e.currentTarget.elements.searchQuery.value;
   newApiServer.resetPege();
   httpsRequest();
   e.target.reset();
+  // Notiflix.Notify.success(`Hooray! We found ${newApiServer.valueInput}.`);  //message
 }
 
 async function httpsRequest() {
   const ara = await newApiServer.fethApiServes();
   const totalHits = Math.ceil(ara.totalHits / newApiServer.per_page);
   const arr = await ara.hits;
+  if (totalHits === newApiServer.page - 1) {
+    Notiflix.Notify.info(
+      `We're sorry, but you've reached the end of search results.`
+    );
+    // clickBtn.classList.add('is-hiden');  //btn load-more
+  }
   if (arr.length === 0) {
     Notiflix.Notify.failure(
-      `Sorry, there are no images matching your search query. Please try again.`
+      `Sorry, there are no ${newApiServer.valueInput} matching your search query. Please try again.`
     );
-  }
-  if (totalHits === newApiServer.page - 1) {
-    console.log(`We're sorry, but you've reached the end of search results.`);
-    clickBtn.classList.add('is-hiden');
   }
   markupGallery(arr);
 }
 
 async function markupGallery(hits) {
-  gallery.insertAdjacentHTML('beforeend', cards(hits));
+  new SimpleLightbox(
+    '.gallery a',
+    gallery.insertAdjacentHTML('beforeend', cards(hits))
+  );
+  observer.observe(jsGuard);
 }
 
 function removeGallery() {
@@ -70,4 +87,12 @@ function controlBtnDisabled(e) {
   } else {
     disabledBtnSearch();
   }
+}
+
+function updateList(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      httpsRequest();
+    }
+  });
 }
